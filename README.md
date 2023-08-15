b51eedfb1c359669b63fb881d27ddca19456db57
bear99a9 <seantrevoredwards@gmail.com>
2023-08-15 18:44:30 +0100

    Arrays and Tuples

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # LearningRust
 
-Stack Vs Heap
+##Stack Vs Heap
 
 Stack is small memory. 
 
@@ -8,4 +8,14 @@ So data to be big for the stack to hold the heap will take it.
 
 Data on the stack is easier to find than on the heap. 
 
-You use pointers to get it from the Heap
+You use pointers to get it from the Heap.
+
+##Data Types
+
+Scalar => Numbers, characters and Booleans
+Rust uses a 4 Byte size allowing for unicode-32 table or 4294967296 total chars
+
+Compound => Arrays, Tuples
+
+Strings
+
--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,20 @@
 
 fn main() {
     let unused_variable: u32 = 0;
+
+    //Array
+    let location_array: [f32;2] = [41.4094069, -81.8546911];
+
+    //Tuple    
+    let location_tuple = ("KCLE", 41.4094069, -81.8546911);
+    // destructuring a tuple
+    let (name, latitude, longitude) = location_tuple;
+
+    println!("Location name: {}, latitude: {}, longitude: {}",
+    location_tuple.0, location_tuple.1, location_tuple.2);
+
+    println!("Location name: {}, latitude: {}, longitude: {}",
+    name, latitude, longitude);
+
     println!("Hello, world!");
 }
