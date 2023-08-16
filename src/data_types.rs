f276d6cebeb7d2741be8bcf42799b6f22be5a26c
bear99a9 <seantrevoredwards@gmail.com>
2023-08-16 12:20:49 +0100

    Declaring variables

---
+++ src/data_types.rs
@@ -0,0 +1,50 @@
+pub fn data_types(){
+    let unused_variable: u32 = 0;
+
+    //Array
+    let location_array: [f32;2] = [41.4094069, -81.8546911];
+
+    //Tuple    
+    let location_tuple = ("KCLE", 41.4094069, -81.8546911);
+    // destructuring a tuple
+    let (name, latitude, longitude) = location_tuple;
+
+    println!("Location name: {}, latitude: {}, longitude: {}",
+    location_tuple.0, location_tuple.1, location_tuple.2);
+
+    println!("Location name: {}, latitude: {}, longitude: {}",
+    name, latitude, longitude);
+
+    //string slice (Immutable)
+    let person_name_slice = "Donald Mallard";
+    
+    // String (Mutable)
+    let person_name_string = person_name_slice.to_string();
+    let person_name_string = "Donald Mallard".to_string();
+    let person_name_string = String::from("Donald Mallard");
+
+    //string slice convert
+    let person_name_slice2 = &person_name_string;
+    let person_name_slice3 = person_name_string.as_str();
+
+    println!("Hello, world!");
+    println!("Hi {}", person_name_slice);
+    println!("Hi {} again", person_name_string);
+
+    //Concatentation will always concat into an immutable string
+    let duck = "Duck";
+    let airline = "Airline";
+
+    let airline_name = [duck, " ", airline].concat();
+
+    let airline_name1 = format!("{} {}", duck, airline);
+
+    let mut slogan = String::new();
+    slogan.push_str("We hit the ground"); // both of these add a string to the string
+    slogan.push(' ');
+    slogan = slogan + "everytime!"; // this adds a string slice not a string to the string
+
+    println!("{}", airline_name);
+    println!("{}", airline_name1);
+    println!("{}", slogan);
+}
---
+++ src/variables.rs
@@ -0,0 +1,11 @@
+pub fn variables(){
+    //super strongly typed
+    let my_variable_name: u32 = 0;
+
+    //inferred
+    let my_inferred_variable = 0; 
+
+    // unused variable fix it with an underscore
+    let _unused_variable = "I dont get used";
+
+}
