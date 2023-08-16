212a434cffb7e23e81c903015889d828f2ce1061
bear99a9 <seantrevoredwards@gmail.com>
2023-08-16 13:46:01 +0100

    Casting

--- src/variables.rs
+++ src/variables.rs
@@ -8,4 +8,21 @@ pub fn variables(){
     // unused variable fix it with an underscore
     let _unused_variable = "I dont get used";
 
+    // casting variables
+
+    let float_thirty_two: f32 = 17.2;
+    let unsigned_eight: u8 = 5;
+
+    // error as we are not explicitly casting
+    //let result = float_thirty_two / unsigned_eight;
+
+    //no error as casting properly
+    let result = float_thirty_two / unsigned_eight as f32;
+    println!("{}", result);
+
+    // cast u8 to char
+    let number: u8 = 65;
+    let letter: char = number as char;
+
+    println!("{}", letter);
 }
