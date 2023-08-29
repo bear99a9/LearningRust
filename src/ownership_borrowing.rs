687676e276ec2084d2ea6142f05a784de2d5d4fb
bear99a9 <seantrevoredwards@gmail.com>
2023-08-29 16:24:22 +0100

    Borrowing and scope of borrowing

--- src/ownership_borrowing.rs
+++ src/ownership_borrowing.rs
@@ -1,12 +1,35 @@
 pub fn ownership_borrowing(){
-    let original = String::from("original value");
+    let mut original = String::from("original value");
     // original is stored on the heap as it is a string 
 
-    println!("\noriginal: \t\"{}\"\n", original);
+    println!("\nOuter original value: \t\"{}\"\n", original);
 
-    let next = original;
-    // when we declare next it points to the data on the heap for original taking ownership of original
+    //let next = original;
+    // when we declare next it points to the data on the heap for original taking over ownership of original
+    // original does therefore not exisit any more of the ownership moves
+    // rust is fast because all the anayalsis happen at compile time not a run time 
 
     // this causes the error: borrow of moved value: `original`
-    println!("{}", original);
+    // => error here println!("{}", original);
+
+    // borrowing allows variable to take temp ownership of the data without deallocating the original variable
+    
+    original = String::from("new value");
+    //here we change the data value and it causes no error as it is before the next borrows it
+
+    //{} allow for scope original takes back ownership of the data once next goes out of scope
+    {
+        let next = &original;
+        // & lets rust now we are borrowing this by allowing it to read the data from the heap location
+    
+        //original = String::from("new value1");
+        //here we change the data value and it causes an error as rust can't guarentee memeory safety anymore
+    
+        println!("Inner scope next: \t\"{}\"", next);
+        println!("Inner scope original: \t\"{}\"", original);
+
+    }
+
+    println!("Outer original value: \t\"{}\"", original);
+
 }
