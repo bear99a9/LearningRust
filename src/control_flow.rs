c3b37a7e2cf1013c58de5a935687ad94acecab3f
bear99a9 <seantrevoredwards@gmail.com>
2023-08-29 10:42:34 +0100

    else if added

--- src/control_flow.rs
+++ src/control_flow.rs
@@ -5,6 +5,9 @@ pub fn control_flow(){
     if word == "Duck" {
         println!("Quack");
     }
+    else if word == "Dog" {
+        println!("Bark");
+    }
     else {
         println!("All quiet out here");
     }
